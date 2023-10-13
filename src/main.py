d83aea9ef6476470d8a3d9617d03d6b306df7b19
joryrod <joryrodriguez25@gmail.com>
2023-10-13 09:56:40 -0500

    examen

--- src/main.py
+++ src/main.py
@@ -3,16 +3,27 @@
 
 # 1. la funcion recibe como parametro un array y devera devolver el numero mayor del array
 def numero_mayor(arr):
-  pass
+  if not arr:
+        return None
+  numero_mayor = arr[0]
+  for num in arr:
+        if num > numero_mayor:
+            numero_mayor = num
+  return numero_mayor
 
 # 2. la funcion recibe un string la funcion devera devolver el string ivertido ejem:entrada=hola, devuleve=aloh
 def inverso(txt):
-  pass
+  return txt[::-1]
 
 # 3. la funcion recibe un string y devuelve si la plabara es un palindromo True si no devuelve False: palindromo que se lee igual de derecha a izquierda y viceversa ejm: entrada=reconocer es palindromo se le igual de izquierda a derecha y viciversa
 def palindromo(txt):
-  pass
+  txt=txt.replace(" ","").lower()
+  return txt==txt[::-1]
 
 # 4. la funcion recibe como parametro una letra y devuelve si la letra ingresada es vocal o consonante el mensaje devera ser si es vocal = 'es vocal' si es consonante 'es consonante'
 def es_vocal(letra):
-  pass
+  vocales = "aeiouAEIOU"   
+  if letra in vocales:
+        return 'es vocal'
+  else:
+      return "es consonante"
